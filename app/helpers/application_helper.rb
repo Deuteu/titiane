c1a7933c2e3f73028e1a6b97471f6ca88bdbee25
Deuteu <Deuteu@users.noreply.github.com>
2015-10-16 10:33:11 +0200

    Add Boostrap 3.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def app_title
+		"AppliTest"
+	end
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title)
+		base_title = app_title
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
+
 end
