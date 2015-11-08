c6d7123a48dd7fca41e234e74b1b8bc720f0b50c
Deuteu <Deuteu@users.noreply.github.com>
2015-11-08 16:06:03 +0100

    Non registrable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable => Sends emails with confirmation instructions and verifies whether an account is already confirmed during sign in
+  # :lockable
+  # :timeoutable
+  # :omniauthable
+  # :registerable => Handles signing up users through a registration process, also allowing them to edit and destroy their account
+  # :recoverable => Resets the user password and sends reset instructions
+  # :validatable => Provides validations of email and password. It's optional and can be customized, so you're able to define your own validations
+  devise :database_authenticatable, :rememberable, :trackable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,32 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  root "static_pages#home"
 
   devise_for :users, controllers: { sessions: "users/sessions" }
+  # Default devise route
+  devise_scope :user do
+    # get "/users/login(.:format)", to: "devise/sessions#new", as: "new_user_session"
+    # post "/users/login(.:format)", to: "devise/sessions#create", as: "user_session"
+    # delete "/users/logout(.:format)", to: "devise/sessions#destroy", as: "destroy_user_session"
+    # post "/users/password(.:format)", to: "devise/passwords#create", as: "user_password"
+    # get "/users/password/new(.:format)", to: "devise/passwords#new", as: "new_user_password"
+    # get "/users/password/edit(.:format)", to: "devise/passwords#edit", as: "edit_user_password"
+    # patch "/users/password(.:format)", to: "devise/passwords#update"
+    # put "/users/password(.:format)", to: "devise/passwords#update"
+    # get "/users/register/cancel(.:format)", to: "devise/registrations#cancel", as: "cancel_user_registration"
+    # post "/users/register(.:format)", to: "devise/registrations#create", as: "user_registration"
+    # get "/users/register/sign_up(.:format)", to: "devise/registrations#new", as: "new_user_registration"
+    # get "/users/edit(.:format)", to: "devise/registrations#edit", as: "edit_user_registration"
+    # patch "/users(.:format)", to: "devise/registrations#update"
+    # put "/users(.:format)", to: "devise/registrations#update"
+    # delete "/users/register(.:format)", to: "devise/registrations#destroy"
+  end
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"#, as: "new_user_session"
+    post "login", to: "devise/sessions#create"#, as: "user_session"
+    delete "logout", to: "devise/sessions#destroy"#, as: "destroy_user_session"
+    get "logout", to: "devise/sessions#destroy"
+  end
 
   get '/home' => 'static_pages#home'
   get '/help' => 'static_pages#help'
