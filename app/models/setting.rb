382039861c6fd5d57ed6870666862a831875bd3e
Deuteu <Deuteu@users.noreply.github.com>
2015-11-27 15:58:40 +0100

    Contact validation. TODO: Errors message display.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,28 @@
 class Setting < ActiveRecord::Base
   self.primary_key = "key"
+  validate :custom_validation
+
+  def valid_format
+    if regex_validation.keys.include?(key)
+      return regex_validation[key][1]
+    end
+  end
+
+  private
+  def custom_validation
+    if regex_validation.keys.include?(key)
+      if not regex_validation[key][0].match(value)
+        errors.add(:value, regex_validation[key][1])
+      end
+    end
+  end
+
+  def regex_validation
+    t = {
+      "mail" => [/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, "Format allowed : mail@domain.xx"],
+      "phone" => [/\A\+{0,1}\d{1,15}\z/, "Format allowed : (+)digits"],
+      "linkedin" => [/^(https?\:\/\/[a-z]{2,3}\.linkedin\.com\/in\/)/, "Format allowed : http(s)://<locale>.linkedin.com/in/..."]
+    }
+    return t
+  end
 end
