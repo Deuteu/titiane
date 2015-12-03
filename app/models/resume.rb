0ad38c12c8be8b6ea8458f71139bd0192b6a2d16
Deuteu <Deuteu@users.noreply.github.com>
2015-12-03 22:50:39 +0100

    Add format indication. Restrict cv extension and size. Fix links to paperclip.

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,5 +3,5 @@ class Resume < ActiveRecord::Base
 
   validates :title, :language, :cv, presence: true
   validates :language, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
-  validates_attachment_content_type :cv, content_type: 'application/pdf'
+  validates_attachment_content_type :cv, content_type: 'application/pdf', :in => 0.megabytes..10.megabytes
 end
