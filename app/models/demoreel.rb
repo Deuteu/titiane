d2543dc43cd918cc9f348574455dcdab16dc739f
Deuteu <Deuteu@users.noreply.github.com>
2016-06-17 19:02:17 +0200

    Fix paperclip config. Add validation on demoreel.

--- app/models/demoreel.rb
+++ app/models/demoreel.rb
@@ -1,3 +1,4 @@
 class Demoreel < ActiveRecord::Base
   default_scope { order(updated_at: :desc) }
+  validates_format_of :link, :with => /\Ahttps?\:\/\/.*/
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,6 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:attachment/:id/:style/:filename'
+Paperclip::Attachment.default_options[:path] = ':rails_root/public:url'
+Paperclip::Attachment.default_options[:url] = '/system/:attachment/:id/:style/:filename'
 #Use :id instead of :id_partition because id shouldn't be large
 #Remove :class because :attachment different among classes
